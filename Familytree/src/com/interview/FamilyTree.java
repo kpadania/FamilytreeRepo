93dd6ed260e7226631cb6c2b760990eb7c07a06c
Karim <kpadania@gmail.com>
2014-10-28 22:33:38 -0400

    Initial frame for person node and requested operations

--- Familytree/src/com/interview/FamilyTree.java
+++ Familytree/src/com/interview/FamilyTree.java
@@ -2,6 +2,17 @@ package com.interview;
 
 public class FamilyTree {
 
+	public void addMember(Person newmember){
+		
+	}
+	public void deleteMember(Person member){
+		
+	}
+	public void renameMember(Person member, String newname){
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
---
+++ Familytree/src/com/interview/Person.java
@@ -0,0 +1,8 @@
+package com.interview;
+
+import java.util.ArrayList;
+
+public class Person {
+   String name;
+   ArrayList<Person> childrens;
+}
